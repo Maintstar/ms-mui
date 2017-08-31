f860e50f7f7532451685bd3100ee14c2e252cae4
Vladimir Buskin <vladimir.buskin@gmail.com>
2017-08-31 12:42:17 +0400

    batch correct

--- update_gh_pages.bat
+++ update_gh_pages.bat
@@ -3,7 +3,7 @@ if %errorlevel% neq 0 exit /b %errorlevel%
 
 git merge master
 call npm run build
-git add .
+git add build -f
 git commit -a -m "rebuilt"
 git push origin gh-pages
 git checkout master
