adf069542998dde95233d5921cbfaa5c3c51c089
Vladimir Buskin <vladimir.buskin@gmail.com>
2017-09-12 13:29:21 +0400

    update gh-pages changed

--- update_gh_pages.bat
+++ update_gh_pages.bat
@@ -1,9 +1,9 @@
 git checkout gh-pages
 if %errorlevel% neq 0 exit /b %errorlevel%
 
-git merge master
+git git reset --hard master
 call npm run build
 git add build -f
 git commit -a -m "rebuilt"
-git push origin gh-pages
+git push origin gh-pages --force
 git checkout master
