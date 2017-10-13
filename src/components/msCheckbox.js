d7886dbd7e82e84cfe0a91bfa1244cdbf33c35d9
Vladimir Buskin <vladimir.buskin@gmail.com>
2017-10-13 14:38:28 +0400

    fix checkbox

--- src/components/msCheckbox.js
+++ src/components/msCheckbox.js
@@ -4,13 +4,13 @@ import { addErrorWarnClasses, initClasses, getClassName } from './ms'
 const getLabel = (p, n) => n ? p + ` (${n})` : p
 
 export default function MSCheckbox(props)  {
-  const { error, warning, className, label, value } = props;
+  const { error, warning, className, label, ...rest } = props;
   let cls = initClasses(className, {'mui-checkbox': 1})
   addErrorWarnClasses(cls, error, warning)
 
   return <div className={getClassName(cls)}>
     <label>
-      <input type="checkbox" name="c21" value={value} />
+      <input type="checkbox" name="c21" {...rest} />
       { getLabel(label, error || warning) }
     </label>
   </div>
